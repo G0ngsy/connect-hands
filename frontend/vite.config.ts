7e87c6556fdb8055b763ecd377f0f9fbaca61d92
sunyoung <gkgkthssla@naver.com>
2026-02-12 13:26:53 +0900

    배포하기

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'site.webmanifest'],
+      manifest: {
+        name: 'Connect·Hands',
+        short_name: 'CH',
+        description: '실시간 수어 AI 통역 서비스',
+        theme_color: '#FFF5F8',
+        background_color: '#FFF5F8',
+        display: 'standalone',
+        icons: [
+          {/* PWA Generator에서 만든 파일명과 일치시켜야 함*/
+            src: 'web-app-manifest-192x192.png', 
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+});
